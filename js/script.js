20ebc4741092f0ad4bed1a3aa6407ada6caa5e1e
zafiraadil <134592181+zafiraadil@users.noreply.github.com>
2023-05-29 09:51:41 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const navbar=document.querySelector(".navbar");
+window.addEventListener("scroll",()=>{
+    if(this.scrollY>0){
+    navbar.classList.remove("py-4");
+    navbar.classList.add("shadow","py-2");
+    }else {
+        navbar.classList.add("py-4");
+        navbar.classList.remove("shadow","py-4");
+
+    }
+})
